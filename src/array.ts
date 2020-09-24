701265197523ca8db5889f13769949a10fa4c03f
nana suzuki <nana.suzuki@nijibox.co.jp>
2020-09-24 15:23:34 +0900

    array

---
+++ src/array.ts
@@ -0,0 +1,13 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings: string[] = ['JS','java','ruby'];
+
+let nijigenHairetsu: number[][] = [
+  [50,100],
+  [150,300]
+]
+
+let hairetshu: (string | number | boolean)[] = [1,false,'java'];
